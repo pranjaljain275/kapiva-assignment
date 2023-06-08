17510f7cb9bf147a7f984728338b5e34614fc542
Pranjal Jain <pranjaljain275@gmail.com>
2023-06-08 17:23:52 +0530

    create different widget for body and list item

--- nodeapp/lib/home.dart
+++ nodeapp/lib/home.dart
@@ -28,8 +28,9 @@ class _HomePageState extends State<Home> {
       isLoading = true;
     });
 
-    const fetchURL = 'https://yif-library-be.onrender.com/book';
-    final res = await http.get(Uri.parse(fetchURL));
+    var fetchURL = 'https://yif-library-be.onrender.com/book';
+    var parseURL = Uri.parse(fetchURL);
+    final res = await http.get(parseURL);
 
     if (res.statusCode == 200) {
       final data = json.decode(res.body);
@@ -73,23 +74,67 @@ class _HomePageState extends State<Home> {
       appBar: AppBar(
         title: const Text('Flutter App'),
       ),
-      body: isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : RefreshIndicator(
-              onRefresh: refreshData,
-              child: ListView.builder(
-                itemCount: dataList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final item = dataList[index];
-                  return ListTile(
-                    title: Text(item['bookName']),
-                    subtitle: Text(item['authorName']),
-                  );
-                },
-              ),
-            ),
+      body: _listBody(),
     );
   }
+
+  Widget _listBody() {
+    if (isLoading == false && dataList.isEmpty) {
+      return const Center(
+        child: Text("Data not found"),
+      );
+    } else if (isLoading == true) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return RefreshIndicator(
+        onRefresh: refreshData,
+        child: ListView.builder(
+          itemCount: dataList.length,
+          itemBuilder: (BuildContext context, int index) {
+            final item = dataList[index];
+            return _displayListData(item);
+          },
+        ),
+      );
+    }
+  }
+}
+
+Widget _displayListData(Map<String, dynamic> item) {
+  return ListTile(
+    // title: Text(item['bookName']),
+    // subtitle: Text(item['authorName']),
+    title: Container(
+      margin: const EdgeInsets.only(
+        bottom: 12,
+      ),
+      child: Center(
+        child: Text(
+          item['bookName'],
+          style: const TextStyle(
+            color: Colors.blue,
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    ),
+    subtitle: Container(
+      margin: const EdgeInsets.only(
+        bottom: 12,
+      ),
+      child: Center(
+        child: Text(
+          item['authorName'],
+          style: const TextStyle(
+            color: Colors.blueGrey,
+            fontSize: 14,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+      ),
+    ),
+  );
 }
